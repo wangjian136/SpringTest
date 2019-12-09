8ec16d4a0719fd9c46ce8a649fd5a1a6c156fddf
wangjian136 <1451518614@qq.com>
2019-12-09 20:04:25 +0800

    spring and hibernate jpa

---
+++ 09-spring-hibernate-jpa/src/test/java/cn/edu/ccut/test/SpringTest.java
@@ -0,0 +1,30 @@
+package cn.edu.ccut.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import cn.edu.ccut.bo.Blog;
+import cn.edu.ccut.service.BlogService;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class SpringTest {
+	
+	@Autowired
+	private BlogService blogService;
+
+	@Test
+	public void testSpringInit() {
+		//System.out.println(blogService.selectAll());
+		
+		//System.out.println(blogService.selectBlogById(101));
+		
+		Blog blog = new Blog();
+		blog.setName("sss");
+		blog.setDescs("sss");
+		blogService.saveBlog(blog);
+	}
+}
